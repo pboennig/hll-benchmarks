058cdff3e84ad6bb42c3387c0237d3571cc9d622
pboennig <pboennig@gmail.com>
2020-06-06 13:08:26 -0700

    Implemented baseline tests for redis-hll
    Test parses text files, spins up processes to run redis-hll estimator,
    and then prints out error and timing information.

--- redis-hll/redis-hll.py
+++ redis-hll/redis-hll.py
@@ -8,17 +8,13 @@ a = parser.parse_args()
 
 r = redis.Redis()
 r.flushdb() # new estimate each time
-uniq = set()
 
 for file in a.files:
     f = open(file, "r")
     for l in f:
         words = l.split()
-        uniq.update(words)
         for word in words:
             r.pfadd("hll", word) 
 
 estimate = r.pfcount("hll")
-print("estimated cardinality:", estimate) 
-print("actual cardinality:", len(uniq))
-print("error: {:.2%}".format(abs(estimate - len(uniq)) / len(uniq)))
+print(estimate) 
---
+++ tests/acc_test.py
@@ -0,0 +1,39 @@
+import shlex, subprocess
+import os
+import time
+
+texts = ["../text/ulysses.txt", "../text/war_and_peace.txt"]
+encoding = "utf-8"
+script_path = os.path.join(os.path.dirname(__file__), "../redis-hll/redis-hll.py")
+
+def parse_files(texts):
+    ground_truth = {}
+    lengths = {}
+    for text in texts:
+        path = os.path.join(os.path.dirname(__file__), text)
+        uniq = set()
+        length = 0
+        with open(path, "r") as f:
+            for l in f: 
+                tok = l.split()
+                uniq.update(tok)
+                length += len(tok)
+        ground_truth[path] = len(uniq)
+        lengths[path] = length
+    return ground_truth, lengths
+
+def test_redis(gt, lengths):
+    redis = subprocess.Popen("redis-server", stdout=subprocess.PIPE)
+    for txt_path, card in gt.items(): 
+        print("Testing file {} with {} tokens ({} unique)".format(txt_path, lengths[txt_path], card))
+        t0 = time.time()
+        estimator = subprocess.Popen(["python3", script_path, txt_path], stdout=subprocess.PIPE)
+        guess = estimator.communicate()[0].decode(encoding).rstrip()
+        t1 = time.time()
+        print("Error: {:.2%}".format(int(guess) / card - 1))
+        print("Took {:.2f} seconds".format(t1-t0))
+    redis.kill()
+
+if __name__=="__main__":
+    gt, lengths = parse_files(texts)
+    test_redis(gt, lengths)
