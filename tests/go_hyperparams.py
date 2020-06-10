bb618ff94dffab196852f4bd42766ad89277a7f9
pboennig <pboennig@gmail.com>
2020-06-10 16:19:04 -0700

    go hyperparam sweep now returns guesses

--- tests/go_hyperparams.py
+++ tests/go_hyperparams.py
@@ -2,10 +2,12 @@
 Sweep over possible hyperparameters in Go
 '''
 
-import acc_test
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+import subprocess
+import acc_test
 
 M_MIN = 4
 M_MAX = 16
@@ -18,35 +20,21 @@ py_path = os.path.join(os.path.dirname(__file__), "../python-hll/python-hll-simp
 
 gt, _ = acc_test.parse_files(texts)
 
-
-def plot_results(res, texts):
-    plt.title("Accuracy")
-    plt.xlabel("b (address size)")
-    plt.ylabel("absolute error (%)")
-    for i in range(len(texts)):
-        f = os.path.split(os.path.splitext(texts[i])[0])[-1]
-        plt.scatter(np.arange(M_MIN, M_MAX+1), res[i,:,0], label=f)
-    plt.legend(loc="upper right")
-    plt.savefig("go_plots/acc.png")
-    plt.clf()
-
-    plt.title("Speed")
-    plt.xlabel("b (address size)")
-    plt.ylabel("time (s)")
-    for i in range(len(texts)):
-        f = os.path.split(os.path.splitext(texts[i])[0])[-1]
-        plt.scatter(np.arange(M_MIN, M_MAX+1), res[i,:,1], label=f)
-    plt.legend(loc="upper right")
-    plt.savefig("go_plots/speed.png")
-    plt.clf()
-
 res = np.zeros((len(texts),M_MAX - M_MIN + 1, 2))
+def estimate(command):
+    t0 = time.time()
+    estimator = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    guess = estimator.communicate()[0].decode(encoding).rstrip()
+    t1 = time.time()
+    t = t1-t0
+    return int(guess), t
+
 for i in range(len(texts)):
     f = list(gt.keys())[i]
     for m in range(M_MIN, M_MAX+1):
         command = ["go", "run", go_path, str(m), f]
-        error, t = acc_test.acc_test(command,gt[f])
-        res[i][m - M_MIN] = abs(error), t
+        res[i][m - M_MIN] = estimate(command)
+
 np.save("go_plots/go_plots.npy", res)
 
 
