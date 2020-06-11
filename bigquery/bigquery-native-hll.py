a0b566f13bf2925078a06ee4a87dc8067d524331
surya-narayanan <surya21@stanford.edu>
2020-06-10 17:03:54 -0700

    split code to create and query the datasets

--- bigquery/bigquery-native-hll.py
+++ bigquery/bigquery-native-hll.py
@@ -5,39 +5,15 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/Surya/Surya/School/CS166/
 
 client = bigquery.Client()
 
-def doesTableExist(project_id, dataset_id, table_id):
-    bq.tables().get(
-        projectId=project_id, 
-        datasetId=dataset_id,
-        tableId=table_id).execute()
-    return True
+job_config = bigquery.LoadJobConfig()
+job_config.source_format = bigquery.SourceFormat.CSV
+job_config.skip_leading_rows = 1
+job_config.autodetect = True
+job_config._properties['load']['schemaUpdateOptions'] = ['ALLOW_FIELD_ADDITION']
 
-#  except HttpError err
-#    if err.resp.status <> 404:
-#       raise
-#    return False
 
 ############################## WAR AND PEACE #################################
 
-# Exact Count.
-QUERY = (
-    'SELECT COUNT(DISTINCT string_field_0) as exact_count  \
-        FROM cs166.war_and_peace')
-query_job = client.query(QUERY)  # API request
-rows = query_job.result()  # Waits for query to finish
-
-for row in rows:
-    print("exact count", row.get('exact_count'))
-
-# Approximate Count.
-
-QUERY = (
-    'SELECT HLL_COUNT.EXTRACT(HLL_COUNT.INIT(string_field_0)) as approx_count FROM cs166.war_and_peace')
-query_job = client.query(QUERY)  # API request
-rows = query_job.result()  # Waits for query to finish
-
-for row in rows:
-    print("approx count", row.get('approx_count'))
 
 
 ############################## SHAKESPEARE #################################
@@ -45,29 +21,24 @@ table_id = "cs166-hll-279503.cs166.shakespeare"
 
 schema = [
     bigquery.SchemaField("words", "STRING"),
+    bigquery.SchemaField("string_field_0", "STRING")
 ]
 
 table = bigquery.Table(table_id, schema=schema)
 
 
-#table = client.create_table(table)  # Make an API request.
+table = client.create_table(table)  # Make an API request.
 
-#print(
-#    "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
-#)
+print(
+    "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
+)
 
-
-
-filename = '/path/to/file.csv'
-dataset_id = 'my_dataset'
-table_id = 'my_table'
+filename = '/Users/Surya/Surya/School/CS166/Project/hll-benchmarks/text/shakespeare.csv'
+dataset_id = 'cs166'
+table_id = 'shakespeare'
 
 dataset_ref = client.dataset(dataset_id)
 table_ref = dataset_ref.table(table_id)
-job_config = bigquery.LoadJobConfig()
-job_config.source_format = bigquery.SourceFormat.CSV
-job_config.skip_leading_rows = 1
-job_config.autodetect = True
 
 with open(filename, "rb") as source_file:
     job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
@@ -75,6 +46,39 @@ with open(filename, "rb") as source_file:
 job.result()  # Waits for table load to complete.
 
 print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+
+
+############################## ULYSSES #################################
+table_id = "cs166-hll-279503.cs166.ulysses"
+
+schema = [
+    bigquery.SchemaField("words", "STRING"),
+    bigquery.SchemaField("string_field_0", "STRING")
+]
+
+table = bigquery.Table(table_id, schema=schema)
+
+
+table = client.create_table(table)  # Make an API request.
+
+print(
+    "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
+)
+
+filename = '/Users/Surya/Surya/School/CS166/Project/hll-benchmarks/text/ulysses.csv'
+dataset_id = 'cs166'
+table_id = 'ulysses'
+
+dataset_ref = client.dataset(dataset_id)
+table_ref = dataset_ref.table(table_id)
+
+with open(filename, "rb") as source_file:
+    job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
+
+job.result()  # Waits for table load to complete.
+
+print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+
 
 
 
---
+++ bigquery/bigquery-queries
@@ -0,0 +1,80 @@
+from google.cloud import bigquery
+from urllib.error import HTTPError
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/Surya/Surya/School/CS166/Project/key_files/surya-bigquery-project-editable-key.json"
+
+client = bigquery.Client()
+
+print('############################## WAR AND PEACE #################################')
+# Exact Count.
+QUERY = (
+    'SELECT COUNT(DISTINCT string_field_0) as exact_count  \
+        FROM cs166.war_and_peace')
+query_job = client.query(QUERY)  # API request
+rows = query_job.result()  # Waits for query to finish
+
+for row in rows:
+    print("exact count", row.get('exact_count'))
+    exact_count = row.get('exact_count')
+
+# Approximate Count.
+
+for precision in range(10, 25):
+    i = precision
+    QUERY = (
+        'SELECT HLL_COUNT.EXTRACT(HLL_COUNT.INIT(string_field_0, %d))\
+            as approx_count FROM cs166.war_and_peace' % i) 
+    query_job = client.query(QUERY)  # API request
+    rows = query_job.result()  # Waits for query to finish
+
+    for row in rows:
+        print("approx count with precision level, %d" % i, row.get('approx_count'))
+        pct_diff = round((((row.get('approx_count')) - exact_count) / exact_count) * 100, 2)
+        print("%% difference with precision level, %d" % i, pct_diff)
+        
+
+print('############################## SHAKESPEARE #################################')
+
+# Exact Count.
+QUERY = (
+    'SELECT COUNT(DISTINCT string_field_0) as exact_count  \
+        FROM cs166.shakespeare')
+query_job = client.query(QUERY)  # API request
+rows = query_job.result()  # Waits for query to finish
+
+for row in rows:
+    print("exact count", row.get('exact_count'))
+
+# Approximate Count.
+
+QUERY = (
+    'SELECT HLL_COUNT.EXTRACT(HLL_COUNT.INIT(string_field_0))\
+         as approx_count FROM cs166.shakespeare')
+query_job = client.query(QUERY)  # API request
+rows = query_job.result()  # Waits for query to finish
+
+for row in rows:
+    print("approx count", row.get('approx_count'))
+
+print('############################## ULYSSES #################################')
+
+# Exact Count.
+QUERY = (
+    'SELECT COUNT(DISTINCT string_field_0) as exact_count  \
+        FROM cs166.ulysses')
+query_job = client.query(QUERY)  # API request
+rows = query_job.result()  # Waits for query to finish
+
+for row in rows:
+    print("exact count", row.get('exact_count'))
+
+# Approximate Count.
+
+QUERY = (
+    'SELECT HLL_COUNT.EXTRACT(HLL_COUNT.INIT(string_field_0))\
+         as approx_count FROM cs166.ulysses')
+query_job = client.query(QUERY)  # API request
+rows = query_job.result()  # Waits for query to finish
+
+for row in rows:
+    print("approx count", row.get('approx_count'))
